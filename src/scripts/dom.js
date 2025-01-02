aecb927542c35f0d60e80cd758b7617641d910af
Derek Hayes <derek2099@gmail.com>
2025-01-02 13:00:20 -0600

    Create setIcon function

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,8 +1,21 @@
+import snow from "../images/snow.png";
+
 const displayWeatherData = (weatherObj) => {
     console.log(weatherObj);
     const location = document.querySelector(".location");
+    const iconNow = document.querySelector(".icon-now");
 
     location.innerHTML = weatherObj.address
+
+    // set to appropriate icon
+    iconNow.setAttribute("src", setIcon(weatherObj.today.icon));
+}
+
+const setIcon = (icon) => {
+    switch(icon) {
+        case "snow":
+            return snow;
+    }
 }
 
 export { displayWeatherData }
