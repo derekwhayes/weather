ba483c1a1e7c4e5dc7ce148a2be082e1a3d2e027
Derek Hayes <derek2099@gmail.com>
2025-01-02 12:01:04 -0600

    Add weather icons

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,6 +1,8 @@
 const displayWeatherData = (weatherObj) => {
     console.log(weatherObj);
-    
+    const location = document.querySelector(".location");
+
+    location.innerHTML = weatherObj.address
 }
 
 export { displayWeatherData }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,30 +15,37 @@ const processWeatherData = (weatherData) => {
     address: (weatherData.resolvedAddress).slice(0, (weatherData.resolvedAddress).lastIndexOf(',')),
     alerts: weatherData.alerts,
     today: {
-      maxTemp: Math.round(weatherData.days[1].feelslikemax),
-      minTemp: Math.round(weatherData.days[1].feelslikemin),
-      temp: Math.round(weatherData.days[1].feelslike),
-      precip: weatherData.days[1].precip,
-      snowDepth: weatherData.days[1].snowdepth,
-      icon: weatherData.days[1].icon
+      maxTemp: Math.round(weatherData.days[0].feelslikemax),
+      minTemp: Math.round(weatherData.days[0].feelslikemin),
+      temp: Math.round(weatherData.days[0].feelslike),
+      precip: weatherData.days[0].precip,
+      precipType: weatherData.days[0].preciptype,
+      snowDepth: weatherData.days[0].snowdepth,
+      icon: weatherData.days[0].icon
     },
     day2: {
+      date: new Intl.DateTimeFormat("en-US", {weekday: "long"}).format(new Date(weatherData.days[1].datetime)),
+      maxTemp: Math.round(weatherData.days[1].feelslikemax),
+      minTemp: Math.round(weatherData.days[1].feelslikemin),
+      precip: weatherData.days[1].precip,
+      precipType: weatherData.days[1].preciptype,
+      icon: weatherData.days[1].icon
+    },
+    day3: {
       date: new Intl.DateTimeFormat("en-US", {weekday: "long"}).format(new Date(weatherData.days[2].datetime)),
       maxTemp: Math.round(weatherData.days[2].feelslikemax),
       minTemp: Math.round(weatherData.days[2].feelslikemin),
-      icon: weatherData.days[2].icon
-    },
-    day3: {
-      date: new Intl.DateTimeFormat("en-US", {weekday: "long"}).format(new Date(weatherData.days[3].datetime)),
-      maxTemp: Math.round(weatherData.days[3].feelslikemax),
-      minTemp: Math.round(weatherData.days[3].feelslikemin),
+      precip: weatherData.days[2].precip,
+      precipType: weatherData.days[2].preciptype,
       icon: weatherData.days[2].icon
     },
     day4: {
-      date: new Intl.DateTimeFormat("en-US", {weekday: "long"}).format(new Date(weatherData.days[4].datetime)),
-      maxTemp: Math.round(weatherData.days[4].feelslikemax),
-      minTemp: Math.round(weatherData.days[4].feelslikemin),
-      icon: weatherData.days[2].icon
+      date: new Intl.DateTimeFormat("en-US", {weekday: "long"}).format(new Date(weatherData.days[3].datetime)),
+      maxTemp: Math.round(weatherData.days[3].feelslikemax),
+      minTemp: Math.round(weatherData.days[3].feelslikemin),
+      precip: weatherData.days[3].precip,
+      precipType: weatherData.days[3].preciptype,
+      icon: weatherData.days[3].icon
     }
   }
 
